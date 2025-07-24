88851cea64ab758578a8515b8eabf76b20edb81a
Greg Sochanik <99419516+gregyoto@users.noreply.github.com>
2025-07-24 06:52:54 +0100

    Update twine-to-yoto.rb

--- Formula/twine-to-yoto.rb
+++ Formula/twine-to-yoto.rb
@@ -2,13 +2,12 @@ class TwineToYoto < Formula
   desc "Convert Twine/Twee documents to TweeJSON and YotoJSON"
   homepage "https://github.com/yotoplay/twine-to-yoto"
   version "1.13.0"
-
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yotoplay/twine-to-yoto/releases/download/v1.13.0/twine2yoto-macos-arm64.zip"
+      url "https://github.com/yotoplay/twine-to-yoto/releases/download/v1.13.1/twine2yoto-macos-arm64-1.13.1.zip"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/yotoplay/twine-to-yoto/releases/download/v1.13.0/twine2yoto-macos-x64.zip"
+      url "https://github.com/yotoplay/twine-to-yoto/releases/download/v1.13.1/twine2yoto-macos-x64-1.13.1.zip"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
